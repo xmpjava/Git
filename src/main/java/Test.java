d3b08b30f7fe49740922b8f190072205266c3faa
xmpjava <3050608872@qq.com>
2022-09-21 13:16:21 +0800

    Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("666");
+        System.out.println("666");
+
+        System.out.println("666");
+
     }
 }
